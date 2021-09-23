8292753568ce24429b5fab4c4623c2123538d125
KurtSpiker <kurt.spiker@gmail.com>
2021-09-23 01:16:24 -0600

    Add files via upload

---
+++ fetcher.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+const request = require('request');
+const fs = require('fs');
+
+const input = process.argv.slice(2);
+const link = input[0];
+const path = input[1];
+
+
+
+const response = request.get(link, (error, response, body) => {
+
+  const file = fs.writeFile(path, body, possibleError => {
+    if (possibleError) {
+      console.error(possibleError);
+      return
+    }
+    let stats = fs.statSync(path);
+    var size = stats.size;
+    console.log(`Downloaded and saved ${size} bytes to ./index.html`)
+  })
+
+})
